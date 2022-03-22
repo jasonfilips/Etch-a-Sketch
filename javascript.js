45ccfb55d01b885f7a4a6816c27b81e04152c27d
jasonfilips <jasonfilipss@yahoo.com>
2022-03-22 18:20:52 +0100

    added comments and added a dynamic resolution for squares

--- javascript.js
+++ javascript.js
@@ -1,27 +1,50 @@
 const container = document.getElementById('container');
+let gridSize = 32;
+let cellsize = 0;
 
+//Creates the grid of specific square size
+function createGrid(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        row.style.cssText = "display: flex; flex-direction: row;"
+        cellsize = (600-gridSize)/gridSize;
 
-//Creates the 16x16 grid
-for (let i = 0; i < 16; i++){
-    let row = document.createElement('div');
-    row.classList.add('row');
-    row.style.cssText = "display: flex; flex-direction: row;"
+        container.appendChild(row);
 
-    container.appendChild(row);
-
-    for (let j = 0; j < 16; j++){
-        let item = document.createElement('div');
-        item.classList.add('item');
-        row.appendChild(item);
+        for (let j = 0; j < gridSize; j++){
+            let item = document.createElement('div');
+            item.classList.add('item');
+            item.addEventListener("mousedown", draw);
+            item.addEventListener("mouseover", draw);
+            row.appendChild(item);
+            item.style.height=`${cellsize}px`;
+            item.style.width= `${cellsize}px`;
+        }
     }
 }
 
-const item = document.querySelectorAll('.item');
 
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener("click",reset);
+
+
+//changes the color if the mouse is hovering over the square and left mouse button down
 function draw(e){
-    e.target.style.cssText = 'background-color: blue;';
+    if (e.type === 'mouseover' && e.buttons != 1){return};
+    e.target.style.backgroundColor = 'white';
 }
 
-for (let i = 0; i < item.length; i++){
-    item[i].addEventListener("mousedown", draw);
+//resets all of the squares back to the color gray
+function reset(e){
+    const items = document.querySelectorAll('.item');
+    for( let i = 0; i < items.length; i++)
+    {
+        items[i].style.backgroundColor = "gray";
+    }
 }
+
+
+
+
+
